5397769a74d3a0e0bc2c3dc00cb66be272d6adb8
Charmaine Runes <charmaine.runes@gmail.com>
2020-10-15 17:36:10 -0500

    Added sample image (CHI311) to RequestCells

--- FOIATracker/FOIATracker/Data/request.swift
+++ FOIATracker/FOIATracker/Data/request.swift
@@ -14,11 +14,13 @@ class Request: CustomDebugStringConvertible {
     
     var department: String
     var description: String
+    var imageUrl: String
     var receivedResponse: Bool = false
     
-    init(for department: String, description: String) {
+    init(for department: String, description: String, imageUrl: String) {
         self.department = department
         self.description = description
+        self.imageUrl = imageUrl
         //self.receivedResponse = receivedResponse
         
     }
--- FOIATracker/FOIATracker/Views/requestCell.swift
+++ FOIATracker/FOIATracker/Views/requestCell.swift
@@ -10,12 +10,20 @@ import UIKit
 class RequestCell: UITableViewCell {
     @IBOutlet weak var requestLabel: UILabel!
     @IBOutlet weak var requestDescription: UILabel!
+    @IBOutlet weak var requestImageView: UIImageView!
     
     var request: Request? {
         didSet {
             self.requestLabel.text = request?.department
             self.requestDescription.text = request?.description
             self.accessoryType = .none
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                let requestImageData = NSData(contentsOf: URL(string: self.request!.imageUrl)!)
+                DispatchQueue.main.async {
+                    self.requestImageView.image = UIImage(data: requestImageData as! Data)
+                }
+            }
         }
     }
     
